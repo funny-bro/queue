b713690e92d2f268f769da54d327c37078ce86e2
waheng chang <wahengchang@gmail.com>
2018-12-30 16:05:56 +0800

    change request to replace curl

--- apis/fetch.js
+++ apis/fetch.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 const pMg = require('../lib/processManager')
+const request = require('request');
 
 const cookieName = process.env.COOKIE_NAME
 const cookieValue = process.env.COOKIE_VALUE
@@ -28,9 +29,40 @@ const section = (cityCode, townCode) => {
   return fetch(`https://${domain}/SetZip_sectList`, {"credentials":"include","headers":{"accept":"application/json, text/javascript, */*; q=0.01","accept-language":"en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7","content-type":"application/x-www-form-urlencoded; charset=UTF-8","x-requested-with":"XMLHttpRequest"},"referrer":`https://${domain}/Home`,"referrerPolicy":"no-referrer-when-downgrade","body":`city_code=${cityCode}&town_code=${townCode}`,"method":"POST","mode":"cors"});
 }
 
-const cmd = (options) => {
-  const {cityCode, townCode, sectCode, landBuild, project = '05'} = options
-  return pMg.execPromise(`curl 'https://${domain}/Cmd_getCmd' -H 'Cookie: ${cookieName}=${cookieValue}' -H 'Origin: https://${domain}' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://${domain}/Home' -H 'X-Requested-With: XMLHttpRequest' -H 'Connection: keep-alive' --data 'project=${project}&city=${cityCode}&town=${townCode}&sectno=${sectCode}&landbuild=${landBuild}&code=&sn_type=onwer&qry_cl=2&menu_cl=2&uid=${uid}&qid=${qid}' --compressed`)
+// special case,
+// html post request
+const cmd = (payload) => {
+  const {cityCode, townCode, sectCode, landBuild, project = '05'} = payload
+  const headers = {
+      'Origin': `https://${domain}`,
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Accept': 'application/json, text/javascript, */*; q=0.01',
+      'Referer': `https://${domain}/Home`,
+      'X-Requested-With': 'XMLHttpRequest',
+      'Connection': 'keep-alive',
+      'Cookie': `${cookieName}=${cookieValue}`
+  };
+
+  const dataString = `project=${project}&city=${cityCode}&town=${townCode}&sectno=${sectCode}&landbuild=${landBuild}&code=&sn_type=onwer&qry_cl=2&menu_cl=2&uid=${uid}&qid=${qid}`;
+
+  const options = {
+      url: `https://${domain}/Cmd_getCmd`,
+      method: 'POST',
+      headers: headers,
+      body: dataString
+  };
+  
+  return new Promise((resolve, reject)=>{
+    request(options, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+          return resolve(body);
+        }
+        return reject(error)
+    });
+  })
 }
 
 const getResult = (W, filePath) => {
