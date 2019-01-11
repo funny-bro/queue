a71b2846fc8bcb8fed16ecd33c7e6e7e3a06d6f3
waheng chang <wahengchang@gmail.com>
2019-01-11 17:31:52 +0800

    added db

---
+++ db/landBuildRecord/model.js
@@ -0,0 +1,19 @@
+const sequelize = require('../init')
+const Sequelize = require('sequelize');
+const SectionModel = require('../section/model')
+
+const LandBuildRecord = sequelize.define('landBuildRecord', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV1,
+    primaryKey: true
+  },
+  landBuild: {type: Sequelize.STRING, allowNull: false },
+  data: {type: Sequelize.STRING, defaultValue: ''},
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
+});
+
+LandBuildRecord.belongsTo(SectionModel)
+
+module.exports = LandBuildRecord
